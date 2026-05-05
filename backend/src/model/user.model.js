ba54d2e7205301367972c0d934f84fab748362c8
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-05-05 21:56:31 +0545

    created user model

---
+++ backend/src/model/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+let User = mongoose.model("User", UserSchema);
+
+export default User;
