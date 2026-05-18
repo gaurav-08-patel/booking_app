69343ef0c49bc39ce33dfdf3e0e3a1c8163d3edb
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-05-18 15:56:45 +0545

    added validateToken function to fetch loggedIn user status

--- frontend/src/contexts/AppContext.jsx
+++ frontend/src/contexts/AppContext.jsx
@@ -1,9 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
 import { createContext, useContext } from "react";
 
 let AppContext = createContext({});
 
 export const AppContextProvider = ({ children }) => {
-    return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+    const validateToken = async () => {
+        let response = await fetch(
+            `${import.meta.env.VITE_API_BASE_URL}/api/auth/validateToken`,
+            {
+                credentials: "include",
+            },
+        );
+
+        let responseBody = await response.json();
+        if (!response.ok) {
+            throw new Error(response.message);
+        }
+
+        return responseBody;
+    };
+
+    let { isError } = useQuery({
+        queryKey: ["validateToken"],
+        queryFn: validateToken,
+        retry: false,
+    });
+
+    return (
+        <AppContext.Provider value={{ isLoggedIn: !isError }}>
+            {children}
+        </AppContext.Provider>
+    );
 };
 
 export const useAppContext = () => useContext(AppContext);
