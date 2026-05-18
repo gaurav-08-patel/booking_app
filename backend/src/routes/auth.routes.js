822172b5ecf644b409bb18ae3940b8cc353d7911
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-05-18 13:35:15 +0545

    created auth route to validateToken

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { loginUser } from "../controllers/auth.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 
-export default router
+router.get("/validateToken", verifyToken, (req, res) => {
+    res.status(200).json({ success: true, userId: req.userId });
+});
+
+export default router;
