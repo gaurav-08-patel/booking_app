bbf0dcad41dc35142bd268862c90c66fbcb9e8ea
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-05-18 22:30:35 +0545

    removed sameSite:none from cookie

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -34,7 +34,6 @@ export const loginUser = async (req, res) => {
     res.cookie("auth_token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "none",
         maxAge: 15 * 24 * 60 * 60 * 1000,
     });
 
