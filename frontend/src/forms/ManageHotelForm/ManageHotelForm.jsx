abcecc716818022e00ae7d3f663434636131dcc0
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-05-26 22:08:39 +0545

    Completed ManageHotelForm

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.jsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.jsx
@@ -1,5 +1,37 @@
+import { FormProvider, useForm } from "react-hook-form";
+import DetailsSection from "./DetailsSection";
+import TypesSection from "./TypesSection";
+import FacilitiesSection from "./FacilitiesSection";
+import GuestSection from "./GuestsSection";
+import ImagesSection from "./ImagesSection";
+
 const ManageHotelForm = () => {
-    return <div>ManageHotelForm</div>;
+    const formMethods = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <FormProvider {...formMethods}>
+            <form
+                className="max-w-4xl mx-auto p-2 flex flex-col gap-8"
+                onSubmit={formMethods.handleSubmit(onSubmit)}
+            >
+                <DetailsSection />
+                <TypesSection />
+                <FacilitiesSection />
+                <GuestSection />
+                <ImagesSection/>
+                <button
+                    type="submit"
+                    className="bg-blue-500 text-white font-semibold py-2 px-4 rounded"
+                >
+                    Submit
+                </button>
+            </form>
+        </FormProvider>
+    );
 };
 
 export default ManageHotelForm;
