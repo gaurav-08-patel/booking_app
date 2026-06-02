b3bf7c1bf6276bb6820e3aa8b5de3088e0370b84
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-06-02 12:43:35 +0545

    added SearchBar to the Layout

--- frontend/src/Layouts/Layout.jsx
+++ frontend/src/Layouts/Layout.jsx
@@ -1,16 +1,20 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import SearchBar from "../components/SearchBar";
 
-const Layout = ({children}) => {
-    return <div className="w-screen flex flex-col min-h-screen">
-        <Header/>
-        <Hero/>
-        <div className="flex-1 container mx-auto">
-            {children}
+const Layout = ({ children }) => {
+    return (
+        <div className="w-screen flex flex-col min-h-screen">
+            <Header />
+            <Hero />
+            <div className="container mx-auto -mt-13 mb-5">
+                <SearchBar />
+            </div>
+            <div className="flex-1 container mx-auto">{children}</div>
+            <Footer />
         </div>
-        <Footer/>
-    </div>;
+    );
 };
 
 export default Layout;
